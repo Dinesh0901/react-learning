b51ae471880266658f94a4905e79d964eb5008c3
Dinesh0901 <dineshbca2021@gmail.com>
2025-09-17 17:27:13 +0530

    changes

--- reduxtoolkittodo/src/App.jsx
+++ reduxtoolkittodo/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1>Learn about redux toolkit</h1>
+      <h1 className='bg-amber-300 text-2xl text-white p-4'>Learn about redux toolkit</h1>
       <AddTodo/>
       <Todos />
     </>
